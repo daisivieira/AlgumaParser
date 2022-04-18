03b9c6aada8b3f98bc359255c17b71fccae348c0
Daísi Priscila <102927281+daisivieira@users.noreply.github.com>
2022-04-17 23:47:25 -0300

    Aula 11 - Analise Semântica/ Parte2 - Exemplo4

---
+++ alguma-semantico/src/main/java/br/ufscar/dc/compiladores/alguma/semantico/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufscar.dc.compiladores.alguma.semantico;
+import br.ufscar.dc.compiladores.alguma.semantico.AlgumaParser.ProgramaContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+/**
+ *
+ * @author daisi
+ */
+public class Principal {
+    public static void main(String args[]) throws IOException {
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        AlgumaLexer lexer = new AlgumaLexer(cs);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        AlgumaParser parser = new AlgumaParser(tokens);
+        ProgramaContext arvore = parser.programa();
+        AlgumaSemantico as = new AlgumaSemantico();
+        as.visitPrograma(arvore);
+        AlgumaSemanticoUtils.errosSemanticos.forEach((s) -> System.out.println(s));
+    }
+}
