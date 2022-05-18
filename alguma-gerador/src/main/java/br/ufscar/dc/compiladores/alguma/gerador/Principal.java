ebd925394706f6a0fd5a3858857143f2650c7802
daisivieira <daisicruzvieira@gmail.com>
2022-05-17 23:20:34 -0300

    Aula 12 - Geração de Código - Parte 1

---
+++ alguma-gerador/src/main/java/br/ufscar/dc/compiladores/alguma/gerador/Principal.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.ufscar.dc.compiladores.alguma.gerador;
+
+import br.ufscar.dc.compiladores.alguma.gerador.AlgumaParser.ProgramaContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+/**
+ *
+ * @author daisi
+ */
+
+public class Principal {
+    public static void main(String args[]) throws IOException {
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        AlgumaLexer lexer = new AlgumaLexer(cs);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        AlgumaParser parser = new AlgumaParser(tokens);
+        ProgramaContext arvore = parser.programa();
+        AlgumaSemantico as = new AlgumaSemantico();
+        as.visitPrograma(arvore);
+        AlgumaSemanticoUtils.errosSemanticos.forEach((s) -> System.out.println(s));
+        
+        if(AlgumaSemanticoUtils.errosSemanticos.isEmpty()) {
+            AlgumaGeradorC agc = new AlgumaGeradorC();
+            agc.visitPrograma(arvore);
+            try(PrintWriter pw = new PrintWriter(args[1])) {
+                pw.print(agc.saida.toString());
+            }
+        }
+    }
+}
